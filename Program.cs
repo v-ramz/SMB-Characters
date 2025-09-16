98bdfb3d5616680401fcc15135b4796e02add1d5
v-ramz <vr2025.pr@gmail.com>
2025-09-15 19:36:23 -0500

    read from data file

--- Program.cs
+++ Program.cs
@@ -22,7 +22,23 @@ else
     List<string> Species = [];
     List<string> FirstAppearances = [];
     List<UInt64> YearsCreated = [];
-    // TODO: create user menu
+    // to populate the lists with data, read from the data file
+    try
+    {
+        StreamReader sr = new(file);
+        // first line contains column headers
+        sr.ReadLine();
+        while (!sr.EndOfStream)
+        {
+            string? line = sr.ReadLine();
+            Console.WriteLine(line);
+        }
+        sr.Close();
+    }
+    catch (Exception ex)
+    {
+        logger.Error(ex.Message);
+    }
     string? choice;
     do
     {
