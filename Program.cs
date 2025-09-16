ed3af7d9b555ded9df7fe8f6920bc5671ff5b6b7
v-ramz <vr2025.pr@gmail.com>
2025-09-15 19:14:04 -0500

    make sure mario.csv file exists

--- Program.cs
+++ Program.cs
@@ -6,6 +6,15 @@ var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassL
 
 logger.Info("Program started");
 
-Console.WriteLine("Hello World!");
+string file = "mario.csv";
+// make sure movie file exists
+if (!File.Exists(file))
+{
+    logger.Error("File does not exist: {File}", file);
+}
+else
+{
+    // TODO: create user menu
+}
 
 logger.Info("Program ended");
